58d45ac8251297c05d7e085fc42a798496b12dc7
5ardina <104732478+5ardina@users.noreply.github.com>
2022-05-04 17:40:50 +0500

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IB Minimap
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.41
 // @description  IB PixelPlanet Minimap
 // @author       IlyaBOT
 // @match        https://pixelplanet.fun/*
